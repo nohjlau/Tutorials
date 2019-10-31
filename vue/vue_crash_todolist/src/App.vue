c5d4688f0f3939157ca9feffcb135ae999c483b5
John Lau <nohjlau@gmail.com>
2019-10-31 14:21:07 -0700

    Implemented fake REST API

--- vue/vue_crash_todolist/src/App.vue
+++ vue/vue_crash_todolist/src/App.vue
@@ -1,8 +1,10 @@
 <template>
   <div id="app">
     <Header />
-    <AddTodo v-on:add-todo="addTodo"/>
-    <Todos v-bind:todos="todos" v-on:del-todo="deleteTodo"/>
+    <div id="body">
+      <AddTodo v-on:add-todo="addTodo"/>
+      <Todos v-bind:todos="todos" v-on:del-todo="deleteTodo"/>
+    </div>
   </div>
 </template>
 
@@ -10,6 +12,7 @@
 import Todos from './components/Todos';
 import Header from './components/layout/Header';
 import AddTodo from './components/AddTodo';
+import axios from 'axios';
 export default {
   name: 'app',
   components: {
@@ -20,31 +23,30 @@ export default {
   data() {
     return {
       todos: [
-        {
-          id: 1,
-          title: "Todo One",
-          completed: false
-        },
-        {
-          id: 2,
-          title: "Todo Two",
-          completed: true
-        },
-        {
-          id: 3,
-          title: "Todo Three",
-          completed: false
-        }
+        
       ]
     }
   },
   methods: {
     deleteTodo(id) {
-      this.todos = this.todos.filter(todo => todo.id !== id);
+      axios.delete(`https://jsonplaceholder.typicode.com/todos/${id}`)
+        .then(this.todos = this.todos.filter(todo => todo.id !== id))
+        .catch();      
     },
     addTodo(newTodo) {
-      this.todos = [...this.todos, newTodo];
+      const { title, completed } = newTodo;
+      axios.post('https://jsonplaceholder.typicode.com/todos', {
+        title,
+        completed
+      })
+        .then(res => this.todos = [...this.todos, res.data])
+        .catch();
     }
+  },
+  created() {
+    axios.get('https://jsonplaceholder.typicode.com/todos?_limit=10')
+      .then(res => this.todos = res.data)
+      .catch()
   }
 }
 </script>
@@ -59,6 +61,12 @@ export default {
   body {
     font-family: Arial, Helvetica, sans-serif;
     line-height: 1.4;
+    margin-left: 30%;
+    margin-right: 30%;
+    margin-top: 50px;
+  }
+  #body {
+    border: 5px solid gray;
   }
   .btn {
     display: inline-block;
