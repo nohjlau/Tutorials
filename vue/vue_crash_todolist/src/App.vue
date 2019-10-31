db597fa74208fb4026bde07c197148aa3f5b6aab
John Lau <nohjlau@gmail.com>
2019-10-31 14:42:51 -0700

    Added vue-router for multiple pages.

--- vue/vue_crash_todolist/src/App.vue
+++ vue/vue_crash_todolist/src/App.vue
@@ -1,82 +1,38 @@
 <template>
   <div id="app">
     <Header />
-    <div id="body">
-      <AddTodo v-on:add-todo="addTodo"/>
-      <Todos v-bind:todos="todos" v-on:del-todo="deleteTodo"/>
-    </div>
+    <router-view/>
   </div>
 </template>
 
 <script>
-import Todos from './components/Todos';
 import Header from './components/layout/Header';
-import AddTodo from './components/AddTodo';
-import axios from 'axios';
 export default {
-  name: 'app',
+  name:"app",
   components: {
-    Header,
-    Todos,
-    AddTodo
-  },
-  data() {
-    return {
-      todos: [
-        
-      ]
-    }
-  },
-  methods: {
-    deleteTodo(id) {
-      axios.delete(`https://jsonplaceholder.typicode.com/todos/${id}`)
-        .then(this.todos = this.todos.filter(todo => todo.id !== id))
-        .catch();      
-    },
-    addTodo(newTodo) {
-      const { title, completed } = newTodo;
-      axios.post('https://jsonplaceholder.typicode.com/todos', {
-        title,
-        completed
-      })
-        .then(res => this.todos = [...this.todos, res.data])
-        .catch();
-    }
-  },
-  created() {
-    axios.get('https://jsonplaceholder.typicode.com/todos?_limit=10')
-      .then(res => this.todos = res.data)
-      .catch()
+    Header
   }
 }
 </script>
-
 <style>
-  * {
-    box-sizing: border-box;
-    margin: 0;
-    padding: 0;
-  }
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: left;
+  color: #2c3e50;
+}
 
-  body {
-    font-family: Arial, Helvetica, sans-serif;
-    line-height: 1.4;
-    margin-left: 30%;
-    margin-right: 30%;
-    margin-top: 50px;
-  }
-  #body {
-    border: 5px solid gray;
-  }
-  .btn {
-    display: inline-block;
-    border: none;
-    background: #555;
-    color: #fff;
-    padding: 6px 20px;
-    cursor: pointer;
-  }
-  .btn:hover {
-    background: #666;
-  }
+#nav {
+  padding: 5px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: gray;
+}
+
+#nav a.router-link-exact-active {
+  color: white;
+}
 </style>
