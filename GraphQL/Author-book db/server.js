d0d4a534cad15095668b516db3405377c8cbac95
John Lau <nohjlau@gmail.com>
2019-12-03 15:29:50 -0800

    GraphQL: Implemented mutations to add books

--- GraphQL/Author-book db/server.js
+++ GraphQL/Author-book db/server.js
@@ -95,8 +95,40 @@ const RootQueryType = new GraphQLObjectType({
     })
 })
 
+const RootMutationType = new GraphQLObjectType({
+    name: 'Mutation',
+    description: 'Root Mutation',
+    fields: () => ({
+        addBook: {
+            type: BookType,
+            description: 'Add a Book',
+            args: {
+                name: { type: GraphQLNonNull(GraphQLString) },
+                authorId: { type: GraphQLNonNull(GraphQLInt) }
+            },
+            resolve: (parentm, args) => {
+                const book = { id: books.length + 1, name: args.name, authorId: args.authorId };
+                books.push(book);
+                return book;
+            }
+        },
+        addAuthor: {
+            type: AuthorType,
+            description: 'Add a Author',
+            args: {
+                name: { type: GraphQLNonNull(GraphQLString) },
+            },
+            resolve: (parentm, args) => {
+                const author = { id: authors.length + 1, name: args.name};
+                authors.push(author);
+                return author;
+            }
+        }
+    })
+})
 const schema = new GraphQLSchema({
-    query: RootQueryType
+    query: RootQueryType,
+    mutation: RootMutationType
 })
 app.use('/graphql', expressGraphQL({
     schema: schema,
