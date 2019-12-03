3efff0268d375c53a0bc88dc5c0f79f60e990e31
John Lau <nohjlau@gmail.com>
2019-12-03 12:41:44 -0800

    Dummy GraphQL schema

--- GraphQL/Author-book db/server.js
+++ GraphQL/Author-book db/server.js
@@ -1,4 +1,26 @@
 const express = require('express');
+const expressGraphQL = require("express-graphql");
+const {
+    GraphQLSchema,
+    GraphQLObjectType,
+    GraphQLString
+} = require('graphql')
+
 const app = express();
 
+const schema = new GraphQLSchema({
+    query: new GraphQLObjectType({
+        name: 'HelloWorld',
+        fields: () => ({
+            message: { 
+                type: GraphQLString,
+                resolve: () => 'Hello World' //Function that tells GraphQL where to get the information
+            }
+        })
+    })
+})
+app.use('/graphql', expressGraphQL({
+    schema: schema,
+    graphiql: true
+}))
 app.listen(5000., () => console.log('Server Running'));
